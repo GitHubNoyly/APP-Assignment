45a0253be624ca70983f8a4762806b18baacdcea
Nigel0 <20198037-Nigel0@users.noreply.replit.com>
2023-04-25 14:55:20 +0000

    flask is now working and jokes are shown on the page

--- main.py
+++ main.py
@@ -7,32 +7,14 @@ app = Flask(__name__)
 def display_homepage():
     return render_template('index.html')
 
-@app.route("/api/journal", methods=['GET'])
-def journal():
+@app.route("/api/jokes", methods=['GET'])
+def joke():
   site_root = os.path.realpath(os.path.dirname(__file__))
   json_url = os.path.join(site_root, "data", "jokes.json")
-  # with keyword deals with closing file etc.
-  with open(json_url, 'r') as openfile:
-    # Reading from json file
-    json_object = json.load(openfile)
-  return json_object
+  file = open(json_url, "r")
+  
+  return file.read()
 
-@app.route("/api/journal", methods=['PUT'])
-def upload():
-  messageOK = jsonify(message="Journals uploaded!")
-  messageFail = jsonify(message="Uploading Journals failed as dats not in JSON format!")
-  if request.is_json:
-    req = request.get_json() # Parse the JSON into a Python dictionary
-    site_root = os.path.realpath(os.path.dirname(__file__))
-    #save json to file
-    json_url = os.path.join(site_root, "data", "jokes.json")
-    with open(json_url, 'w') as openfile: # with keyword deals with closing file
-      json.dump(req, openfile)
-      # Return a string along with an HTTP status code
-    return messageOK, 200
-  else:
-    # The request body wasn't JSON so return a 400 HTTP status code
-    return messageFail, 400
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,13 +1,8 @@
-//journal event handlers
-document.getElementById("listEntries").addEventListener('click', populateEntry);
-document.getElementById("btnDeleteEntry").addEventListener('click', deleteEntry);
-document.getElementById("btnAddEntry").addEventListener('click', addEntry);
-document.getElementById("btnUploadJournal").addEventListener('click', uploadJournal);
 
-// initialise journal list
+// initialise joke list
 document.addEventListener("DOMContentLoaded", function(){
- console.log("calling getJournal");
- getJournalEntries();
+ console.log("calling getJoke");
+ getJokeEntries();
 });
 
 //utility functions - DO NOT EDIT OR DELETE
@@ -17,20 +12,21 @@ function getUniqueKey(){
  );
 };
 
-function getJournalEntries(){
+function getJokeEntries(){
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
   console.log(this.readyState, this.status);
+    
   if (this.readyState == 4 && this.status == 200) {
     console.log("success");
-    let journalResult = JSON.parse(this.responseText);
+    let jokeResult = JSON.parse(this.responseText);
     const jokes = [];
-    for (let item of journalResult.jokes) {
-      jokes.push([String(item.entryDate), String(item.author), String(item.joke), String(item.punchline)]);
+    for (let item of jokeResult.jokes) {
+      jokes.push([String(item.joke) + " " + String(item.punchline)]);
     }
-      console.log(jokes);
       jokes.sort(() => Math.random() - 0.5);
       for (let i = 0; i <= 5; i++) {
+        console.log(jokes[i]);
         document.getElementById("placeholder"+(i+1)).innerHTML = jokes[i];
       }
     }
@@ -38,92 +34,7 @@ function getJournalEntries(){
       console.log("xhttp request problem occurred")
     }
   }
-  xhttp.open("GET", "api/journal", true);
+  xhttp.open("GET", "/api/jokes", true);
   xhttp.send();
 }
 
-function clearEntry(){
- document.getElementById("idEntry").value = "";
- document.getElementById("dateEntry").value = "";
- document.getElementById("namEntry").value = "";
- document.getElementById("txtNote").value = "";
-}
-
-function populateEntry(e){
-clearEntry()
-  let itemIndex = e.target.id
-  let itemDate = e.target.getAttribute("dat");
-  let itemName = e.target.getAttribute("name");
-  let itemNote = e.target.getAttribute("note");
-  document.getElementById("idEntry").value = itemIndex;
-  document.getElementById("dateEntry").value = itemDate;
-  document.getElementById("namEntry").value = itemName;
-  document.getElementById("txtNote").value = itemNote;
-}
-
-function addEntry(){
-  let uid = getUniqueKey();
-  const dat = new Date();
-  let newDate = dat.getDate() + "/" + dat.getMonth() + "/" + dat.getFullYear();
-  let newName = document.getElementById("nameAdd").value;
-  let newNote = document.getElementById("txtAdd").value;
-  if(newName == "" || newNote == ""){
-  alert("Please enter values in the name and the notes inputs.")
-  }else{
-    let newEntry = document.createElement('li');
-    newEntry.id = uid;
-    newEntry.setAttribute("dat", newDate)
-    newEntry.setAttribute("name", newName);
-    newEntry.setAttribute("note", newNote);
-    newEntry.innerText = newDate;
-    document.getElementById("listEntries").appendChild(newEntry)
-    alert("Journal entry added to clientsdide list. Upload to save the list.")
-    }
-  }
-
-function deleteEntry(){
- let idToDelete = document.getElementById("idEntry").value;
- if(idToDelete != ""){
-   document.getElementById(idToDelete).remove();
-   //remove deleted details from selected entry boxes
-   clearEntry()
-   alert("Journal entry deleted on clientside. Upload to save changes.")
-  } else {
-    alert("Please select an entry to delete.")
-  }
-}
-
-function uploadJournal(){
-  let uploadList = document.getElementById("listEntries");
-  var entriesList = uploadList.getElementsByTagName("li")
-  //console.log("entries no. " + entriesList.length)
-  // make object to convert to JSON
-  let uploadObject = {};
-  uploadObject.journals = [];
-  //list items and put into an array of objects
-  //console.log(entriesList)
-  for (let i = 0; i < entriesList.length ; i++){
-    //console.log("upload entry " + entriesList[i].innerHTML);
-    let objEntry = {}
-    objEntry.date = entriesList[i].getAttribute("dat");
-    objEntry.name = entriesList[i].getAttribute("name");
-    objEntry.note = entriesList[i].getAttribute("note");
-    uploadObject.journals.push(objEntry);
-  }
-  //console.log("upload Object:" + JSON.stringify(uploadObject));
-  //console.log(uploadObject.journals[0])
-  //convert object to JSON and PUT to api
-  let xhttp = new XMLHttpRequest();
-  let url = "/api/journal";
-  xhttp.onreadystatechange = function() {
-    let strResponse = "Error: no response";
-    if (this.readyState == 4 && this.status == 200) {
-      strResponse = JSON.parse(this.responseText);
-      alert(strResponse.message)
-    }
-  };
-  xhttp.open("PUT", url, true);
-  var data = JSON.stringify(uploadObject)
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.send(data);
-}
