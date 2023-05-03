786577a929e9a7adb2366cd5cac069c0cc4901ea
Nigel0 <20198037-Nigel0@users.noreply.replit.com>
2023-05-03 13:48:13 +0000

    introducing the PUT commands and selecting of the table

--- main.py
+++ main.py
@@ -3,21 +3,37 @@ import sys, json, os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def display_homepage():
-    return render_template('index.html')
+  return render_template('index.html')
+
 
 @app.route("/api/jokes", methods=['GET'])
 def joke():
   site_root = os.path.realpath(os.path.dirname(__file__))
   json_url = os.path.join(site_root, "data", "jokes.json")
   file = open(json_url, "r")
-  
+
   return file.read()
+
+@app.route("/api/jokes", methods=['PUT'])
+def upload():
+  messageOK = jsonify(message="Joke Succsesfully Uploaded")
+  messageFail = jsonify(message="Upload Failed")
+  if request.is_json:
+    req = request.get_json()  # Parse the JSON into a Python dictionary
+    site_root = os.path.realpath(os.path.dirname(__file__))
+    #save json to file
+    json_url = os.path.join(site_root, "data", "jokes.json")
+    with open(json_url, 'w') as openfile:  # with keyword deals with closing file
+      json.dump(req, openfile)
+      # Return a string along with an HTTP status code
+    return messageOK, 200
+  else:
+    # The request body wasn't JSON so return a 400 HTTP status code
+    return messageFail, 400
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
-
-
-
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -5,13 +5,6 @@ document.addEventListener("DOMContentLoaded", function(){
  getJokeEntries();
 });
 
-//utility functions - DO NOT EDIT OR DELETE
-function getUniqueKey(){
- return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
- (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
- );
-};
-
 function getJokeEntries(){
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -20,15 +13,11 @@ function getJokeEntries(){
   if (this.readyState == 4 && this.status == 200) {
     console.log("success");
     let jokeResult = JSON.parse(this.responseText);
-    const jokes = [];
+    const displayJokes = [];;
     for (let item of jokeResult.jokes) {
-      jokes.push([String(item.joke) + " " + String(item.punchline)]);
+      displayJokes.push([[String(item.entryDate)], [String(item.author)], [String(item.joke)],[String(item.punchline)]]);
     }
-      jokes.sort(() => Math.random() - 0.5);
-      for (let i = 0; i <= 5; i++) {
-        console.log(jokes[i]);
-        document.getElementById("placeholder"+(i+1)).innerHTML = jokes[i];
-      }
+      makeJokeTable(displayJokes);
     }
    else{
       console.log("xhttp request problem occurred")
@@ -38,3 +27,111 @@ function getJokeEntries(){
   xhttp.send();
 }
 
+function makeJokeTable(displayJokes) {
+
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+
+  // creating all cells
+  for (let i = 0; i < displayJokes.length; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < 4; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(displayJokes[i][j]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+  tbl.classList.add("unSelectedRow");
+
+   tableLoad(displayJokes);
+  
+  displayJokes.sort(() => Math.random() - 0.5);
+  for (let i = 0; i <= 5; i++) {
+    console.log(displayJokes[i]);
+    document.getElementById("placeholder"+(i+1)).innerHTML = displayJokes[i][2] + "  " + displayJokes[i][3];
+  }
+}
+
+
+function tableLoad(displayJokes){
+  var myTable = document.getElementsByTagName("tr");
+  console.log(myTable[0])
+  function selectJokeRow(joke) {
+    console.log("a row has been selected");
+    var rowElement = joke.target.parentElement;
+    if (rowElement.className == "selectedRow") {
+       rowElement.classList.remove("selectedRow");
+       rowElement.classList.add("unSelectedRow");
+    }
+    else if (rowElement.id !== "pop_table" || rowElement.tagName !== "body") {
+       rowElement.classList.remove("unSelectedRow");
+       rowElement.classList.add("selectedRow");
+    }
+  }
+  for (let i = 0; i < displayJokes.length; i++) {
+      myTable[i].addEventListener("click", selectJokeRow, false);
+  }
+
+}
+
+function uploadJokes(){
+  //get list 
+  let uploadList = document.getElementsByClassName("add_joke_inputs");
+  var entriesList = document.getElementsByTagName("input");
+  //console.log("entries no. " + entriesList.length)
+  // make object to convert to JSON
+  let uploadObject = {};
+  uploadObject.journals = [];
+  //list items and put into an array of objects
+  //console.log(entriesList)
+  for (let i = 0; i < entriesList.length ; i++){
+    //console.log("upload entry " + entriesList[i].innerHTML);
+    let objEntry = {}
+    objEntry.entryDate = entriesList[i].getAttribute("eDate");
+    objEntry.author = entriesList[i].getAttribute("author");
+    objEntry.joke = entriesList[i].getAttribute("joke");
+    objEntry.punchline = entriesList[i].getAttribute("pLine");
+    uploadObject.journals.push(objEntry);
+  }
+  //console.log("upload Object:" + JSON.stringify(uploadObject));
+  //console.log(uploadObject.journals[0])
+
+  //convert object to JSON and put to api
+  let xhttp = new XMLHttpRequest();
+  let url = "/api/jokes"
+  
+    xhttp.onreadystatechange = function() {
+      let strResponse = "Error: no response";
+      if (this.readyState == 4 && this.status == 200) {
+        strResponse = JSON.parse(this.responseText);
+        alert(strResponse.message)
+      }
+      //document.getElementById(elResponse).setAttribute("value",  strResponse.result);
+      
+    };
+    xhttp.open("PUT", url, true);
+    // Converting JSON data to string
+    var data = JSON.stringify(uploadObject)
+    // Set the request header i.e. which type of content you are sending
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    //send it
+    xhttp.send(data);
+
+}
